9d1f0dd1b3d05af766e790d1157a238a2b18fa22
bdamadi <bdamadi@gmail.com>
2021-07-24 23:35:32 +0700

    Test pre-trained model

--- pretrained_example.py
+++ pretrained_example.py
@@ -14,14 +14,17 @@ import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
 import config
+from training import misc
 
 def main():
     # Initialize TensorFlow.
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    # url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
+    # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    url = '/content/drive/MyDrive/LSUN/karras2019stylegan-bedrooms-256x256.pkl'
+    with misc.open_file_or_url(url) as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
         # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
